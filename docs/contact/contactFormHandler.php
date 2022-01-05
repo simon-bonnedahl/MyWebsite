af72a3167252f86bd5c34db3a83c924ff8edc47f
CheQita <simon2.bonnedahl@gmail.com>
2022-01-05 01:14:26 +0100

    init

--- docs/contact/contactFormHandler.php
+++ docs/contact/contactFormHandler.php
@@ -8,18 +8,16 @@
     $email_subject = "Contact Form Submission";
 
     $email_body = "User Name:   $name.\n".
-                  "User Email: $email.\n".
+                  "User Email: $visitor_email.\n".
                   "User Message: $message.\n".
     
     $to = "simon.bonnedahl@gmail.com";
 
     $headers = "From: $email_from \r\n";
 
+    $headers .= "Reply: $visitor_email \r\n";
+
     mail($to,$email_subject,$email_body,$headers);
 
     header("Location: contact.html");
-
-                  
-
-
 ?>
